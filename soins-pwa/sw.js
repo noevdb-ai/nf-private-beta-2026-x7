b2198aa4071ca0d0b81bc80ea5ff4aabffb3c57e
noevdb-ai <noevdb@hotmail.be>
2026-02-20 09:38:35 +0100

    Delete soins-pwa directory

--- soins-pwa/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("nurseflow").then(cache =>
-      cache.addAll(["./","./index.html","./styles.css","./app.js"])
-    )
-  );
-});
-
