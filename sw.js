9bfcf64e23015f8ed14897b60838a500a755a7d4
noevdb-ai <noevdb@hotmail.be>
2026-04-30 10:08:05 +0200

    Update sw.js
    Mise à jour

--- sw.js
+++ sw.js
@@ -1,8 +1,14 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("nurseflow").then(cache =>
-      cache.addAll(["./","./index.html","./styles.css","./app.js"])
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./styles.css",
+        "./app.js",
+        "./assets/picc_steps.png",
+        "./assets/picc_tuto.mp4"
+      ])
     )
   );
 });
-
